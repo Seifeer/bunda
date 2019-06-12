9c5a22f74eb5fc3e3fc309a1fa9b9c1733b77247
you <you@exemplo.com>
2019-06-12 15:29:33 -0300

    bunda suja

--- app/src/main/java/ifpi/hello4662019/ListagemAnunciosActivity.java
+++ app/src/main/java/ifpi/hello4662019/ListagemAnunciosActivity.java
@@ -1,11 +1,20 @@
 package ifpi.hello4662019;
 
+import android.media.MediaPlayer;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import ifpi.hello4662019.adapters.ListagemAnunciosAdapter;
 import ifpi.hello4662019.modelos.Anuncio;
@@ -35,6 +44,24 @@ public class ListagemAnunciosActivity extends AppCompatActivity {
         rvAnuncios.setLayoutManager(new LinearLayoutManager(this));
 
     }
+
+     private void obterAnunciosDoFirestore(){
+
+        final List<Anuncio> anuncios = new ArrayList<>();
+
+        db.collection("anuncios").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>(){
+
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task){
+                if(task.isSuccessful()){
+                    for (DocumentSnapshot documento: task.getResult()){
+                        Anuncio anuncio = documento.toObject(Anuncio.class);
+                        anuncios.add(anunci
+                    }
+                }
+            }
+        });
+     }
 }
 
 
